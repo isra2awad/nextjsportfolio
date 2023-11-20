1569f97e97dc3d1c7197125a3db0e968a1608cf5
Isra Adel <israsalama4@gmail.com>
2023-11-20 17:08:19 +0400

    adding intro buttons

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,8 +2,12 @@
 
 import Image from "next/image";
 import profile from "@/public/profile.jpeg";
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 import { Space_Mono } from "next/font/google";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import Link from "next/link";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -58,6 +62,50 @@ export default function Intro() {
         mastering the
         <span className="italic"> MERN stack</span>
       </motion.p>
+
+      <motion.div
+        className=" flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full
+           outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          href="#contact"
+        >
+          {" "}
+          Message Me{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full
+         outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer"
+        >
+          {" "}
+          Download my CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="group bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full
+          outline-none focus:scale-110 hover:scale-110 `active:scale-105 transition"
+          href=""
+        >
+          {" "}
+          <BsLinkedin className="opacity-70 group-hover:translate-x-1" />{" "}
+        </a>
+
+        <a
+          className="group bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full
+          outline-none focus:scale-110 hover:scale-110 active:scale-105 transition"
+          href=""
+        >
+          {" "}
+          <FaGithubSquare className="opacity-70 group-hover:translate-x-1" />{" "}
+        </a>
+      </motion.div>
     </section>
   );
 }
